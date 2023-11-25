68ec345c2277311050f07bafad9b6d1203702efa
Aura <coolboy1384@gmail.com>
2023-11-24 23:07:13 -0500

    API endpoints
    API endpoints for creating and validating meeting rooms. Rooms expire after 1800 seconds (30 mins)

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,27 +1,65 @@
 import os
 import requests
-from flask import Flask
-from flask_cors import CORS
+from flask import Flask, request
 
 app = Flask(__name__)
-CORS(app)
 
 # Metered Secret Key
 METERED_SECRET_KEY = os.environ.get("METERED_SECRET_KEY")
 # Metered Domain
 METERED_DOMAIN = os.environ.get("METERED_DOMAIN")
 
+url = "https://" + METERED_DOMAIN + "/api/v1/room/" + "?secretKey=" + METERED_SECRET_KEY
+
 
 # API Route to create a meeting room
 @app.route("/api/create/room", methods=['POST'])
 def create_room():
-    return "Create Meeting Room"
+    payload = {
+    "privacy": "public",
+    "ejectAtRoomExp": False,
+    "notBeforeUnixSec": 0,
+    "maxParticipants": 0,
+    "autoJoin": True,
+    "enableRequestToJoin": True,
+    "enableChat": True,
+    "enableScreenSharing": True,
+    "joinVideoOn": False,
+    "joinAudioOn": False,
+    "recordRoom": True,
+    "ejectAfterElapsedTimeInSec": 0,
+    "meetingJoinWebhook": "string",
+    "endMeetingAfterNoActivityInSec": 1800,  # Example value for 5 minutes
+    "audioOnlyRoom": False
+}
+
+    headers = {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+}
+        
+    r = requests.post(url, json=payload, headers=headers)
+    return r.json()
 
 
 # API Route to validate meeting
 @app.route("/api/validate-meeting")
 def validate_meeting():
-    return "Validate Meeting"
+    roomName = request.args.get("roomName")
+    if roomName:
+        r = requests.get("https://" + METERED_DOMAIN + "/api/v1/room/" +
+                         roomName + "?secretKey=" + METERED_SECRET_KEY)
+        
+        data = r.json()
+        if (data.get("roomName")):
+            return {"roomFound": True}
+        else:
+            return {"roomFound": False}
+    else:
+        return {
+            "success": False,
+            "message": "Please specify roomName"
+        }
 
 
 # API Route to fetch the Metered Domain
@@ -33,33 +71,3 @@ def get_metered_domain():
 @app.route("/")
 def index():
     return "Backend"
-
-# API Route to create a meeting room
-@app.route("/api/create/room", methods=['POST'])
-def create_room():
-    r = requests.post("https://"+METERED_DOMAIN + "/api/v1/room" +
-                      "?secretKey="+METERED_SECRET_KEY)
-    return r.json()
-
-# API Route to validate meeting
-@app.route("/api/validate-meeting")
-def validate_meeting():
-    roomName = request.args.get("roomName")
-    if roomName:
-        r = requests.get("https://" + METERED_DOMAIN + "/api/v1/room/" +
-                         roomName + "?secretKey=" + METERED_SECRET_KEY)
-        data = r.json()
-        if (data.get("roomName")):
-            return {"roomFound": True}
-        else:
-            return {"roomFound": False}
-    else:
-        return {
-            "success": False,
-            "message": "Please specify roomName"
-        }
-        
-# API Route to fetch the Metered Domain
-@app.route("/api/metered-domain")
-def get_metered_domain():
-    return {"METERED_DOMAIN": METERED_DOMAIN}
---
+++ webRTC.py
@@ -0,0 +1,3 @@
+import requests
+import flaskr.__init__ as rtc
+
