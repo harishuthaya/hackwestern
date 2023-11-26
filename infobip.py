69caccefb87e04d73d04003feaf4e860ee972988
Aura <coolboy1384@gmail.com>
2023-11-26 03:52:51 -0500

    testing message sending & recieving

--- infobip.py
+++ infobip.py
@@ -2,6 +2,7 @@ import requests
 import json
 import http
 import os
+import http.client
 
 BASE_URL = "5yepqg.api.infobip.com"
 API_KEY = "App 62c9fac6d1e9b6f0ff03f5748b70c41d-fcdd5640-3818-462d-8fbb-62092496adcb"
@@ -29,3 +30,19 @@ def sms(phonenum):
     res = conn.getresponse()
     data = res.read()
     print(data.decode("utf-8"))
+    
+def getresp():
+    conn = http.client.HTTPSConnection("5yepqg.api.infobip.com")
+    payload = ''
+    headers = {
+        'Authorization': API_KEY,
+        'Accept': 'application/json'
+    }
+    conn.request("GET", "/sms/1/inbox/reports", payload, headers)
+    res = conn.getresponse()
+    data = res.read()
+    print(data.decode("utf-8"))
+
+
+#test
+sms('16472368470')
